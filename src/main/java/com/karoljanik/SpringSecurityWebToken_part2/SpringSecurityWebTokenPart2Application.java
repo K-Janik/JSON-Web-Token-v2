c323b3fe8e1b6b1785573682c2efaf0da86c30c9
Karol Janik <karol.jnk@gmail.com>
2020-12-12 19:42:43 +0100

    12.12.2020

---
+++ src/main/java/com/karoljanik/SpringSecurityWebToken_part2/SpringSecurityWebTokenPart2Application.java
@@ -0,0 +1,13 @@
+package com.karoljanik.SpringSecurityWebToken_part2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWebTokenPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWebTokenPart2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/karoljanik/SpringSecurityWebToken_part2/SpringSecurityWebTokenPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karoljanik.SpringSecurityWebToken_part2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWebTokenPart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
